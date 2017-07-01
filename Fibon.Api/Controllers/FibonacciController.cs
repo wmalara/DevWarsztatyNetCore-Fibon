6a6ce8642edd88c6ff51d4fcb89cc401f0660b57
Wojciech Malara <malara.wojciech@gmail.com>
2017-07-01 10:04:58 +0200

    Initial solution structure

---
+++ Fibon.Api/Controllers/FibonacciController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Fibon.Api.Controllers
+{
+    [Route("[controller]")]
+    public class FibonacciController : Controller
+    {
+        [HttpGet("{number}")]
+        public IActionResult Get(int number)
+        {
+            return Content("0");
+        }
+
+        [HttpPost("{number}")]
+        public IActionResult Post(int number)
+        {
+            return Accepted($"fibonacci/{number}", null);
+        }
+    }
+}
